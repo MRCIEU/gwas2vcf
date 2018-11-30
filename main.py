0677890611ff55ef5c48de316ee3bab5a8a7410d
Matthew Lyon <ml@eduroam-43-68.nomadic.bris.ac.uk>
2018-11-30 14:54:08 +0000

    moved json to header

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from gwas_results import GwasResult
 from vcf import Vcf
 import pysam
 from harmonise import Harmonise
-import json
-import os
+from datetime import datetime
+import git
+import sys
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 
 def main():
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+
     parser = argparse.ArgumentParser(description='Map GWAS summary statistics to VCF')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.format(sha))
     parser.add_argument('-o', dest='out', required=True, help='Path to output VCF')
     parser.add_argument('-g', dest='gwas', required=True, help='Path to GWAS tab-sep summary stats')
     parser.add_argument('-f', dest='fasta', required=True, help='Path to reference FASTA')
+    parser.add_argument('-b', dest='build', required=True, help='FASTA build assembly')
     parser.add_argument('-s', dest='skip', default=0, type=int, required=False, help='Number of rows to skip')
     parser.add_argument('-m', dest='max_missing', default=0.05, type=float, required=False,
                         help='Maximum fraction of missing variants permitted')
@@ -34,9 +40,6 @@ def main():
     parser.add_argument('-nea_af_field', dest='nea_af_field', type=int, required=False,
                         help='None effect allele frequency field')
     args = parser.parse_args()
-
-    # load fasta fai
-    fasta = pysam.FastaFile(args.fasta)
 
     # read in GWAS and harmonise alleles to reference fasta
     gwas, total_variants = GwasResult.read_from_text_file(
@@ -61,44 +64,30 @@ def main():
     for i in range(10):
         logging.info("Mapped line {}: {}".format(i, gwas[i]))
 
-    # harmonise to FASTA
-    harmonised, flipped_variants = Harmonise.align_gwas_to_fasta(gwas, fasta)
+    with pysam.FastaFile(args.fasta) as fasta:
 
-    logging.info("Variants harmonised: {}".format(len(harmonised)))
-    logging.info("Variants discarded during harmonisation: {}".format(len(gwas) - len(harmonised)))
-    logging.info("Alleles switched: {}".format(flipped_variants - (len(gwas) - len(harmonised))))
+        # harmonise to FASTA
+        harmonised, flipped_variants = Harmonise.align_gwas_to_fasta(gwas, fasta)
 
-    # check number of skipped is acceptable
-    logging.info("Skipped {} of {}".format(total_variants - len(harmonised), total_variants))
-    if (total_variants - len(harmonised)) / total_variants > args.max_missing:
-        raise RuntimeError("Too many sites skipped. The alleles must be on the forward strand.")
+        logging.info("Variants harmonised: {}".format(len(harmonised)))
+        logging.info("Variants discarded during harmonisation: {}".format(len(gwas) - len(harmonised)))
+        logging.info("Alleles switched: {}".format(flipped_variants - (len(gwas) - len(harmonised))))
 
-    # write to vcf
-    Vcf.write_to_file(harmonised, args.out, fasta)
+        # check number of skipped is acceptable
+        logging.info("Skipped {} of {}".format(total_variants - len(harmonised), total_variants))
+        if (total_variants - len(harmonised)) / total_variants > args.max_missing:
+            raise RuntimeError("Too many sites skipped. The alleles must be on the forward strand.")
 
-    # write metrics to json
-    metrics = {
-        'args': dict(),
-        'counts': dict()
-    }
-    name, ext = os.path.splitext(args.out)
-
-    # add args
-    for arg in vars(args):
-        metrics['args'][arg] = getattr(args, arg)
-
-    # add counts
-    metrics['counts']['total_variants'] = total_variants
-    metrics['counts']['variants_not_read'] = total_variants - len(gwas)
-    metrics['counts']['harmonised_variants'] = len(harmonised)
-    metrics['counts']['variants_not_harmonised'] = len(gwas) - len(harmonised)
-    metrics['counts']['switched_alleles'] = flipped_variants - (len(gwas) - len(harmonised))
-
-    # write to file
-    with open(name + ".json", 'w') as f:
-        json.dump(metrics, f, ensure_ascii=False)
-
-    fasta.close()
+        # write to vcf
+        Vcf.write_to_file(harmonised, args.out, fasta, args.build, {
+            'gwas_harmonisation_command': ' '.join(sys.argv[1:]),
+            'file_date': datetime.now().isoformat(),
+            'total_variants': total_variants,
+            'variants_not_read': total_variants - len(gwas),
+            'harmonised_variants': len(harmonised),
+            'variants_not_harmonised': len(gwas) - len(harmonised),
+            'switched_alleles': flipped_variants - (len(gwas) - len(harmonised))
+        })
 
 
 if __name__ == "__main__":
--- vcf.py
+++ vcf.py
@@ -1,7 +1,5 @@
 import pysam
 import logging
-from datetime import datetime
-import git
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -9,26 +7,28 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(mess
 class Vcf:
 
     @staticmethod
-    def write_to_file(gwas_results, path, fasta):
+    def write_to_file(gwas_results, path, fasta, build, params=None):
         logging.info("Writing to VCF: {}".format(path))
-        repo = git.Repo(search_parent_directories=True)
-        sha = repo.head.object.hexsha
 
         header = pysam.VariantHeader()
         header.add_line(
             '##INFO=<ID=B,Number=A,Type=Float,Description="Effect size estimate relative to the alternative allele(s)">')
         header.add_line('##INFO=<ID=SE,Number=A,Type=Float,Description="Standard error of effect size estimate">')
-        header.add_line('##INFO=<ID=P,Number=A,Type=Float,Description="P-value for effect estimate">')
+        header.add_line('##INFO=<ID=PVAL,Number=A,Type=Float,Description="P-value for effect estimate">')
         header.add_line('##INFO=<ID=AF,Number=A,Type=Float,Description="Alternate allele frequency">')
-        header.add_line('##INFO=<ID=N,Number=A,Type=Float,Description="Total number of samples">')
+        header.add_line('##INFO=<ID=N,Number=A,Type=Float,Description="Sample size used to estimate genetic effect">')
 
         # add contig lengths
         assert len(fasta.references) == len(fasta.lengths)
         for n, contig in enumerate(fasta.references):
-            header.add_line("##contig=<ID={},length={}>".format(contig, fasta.lengths[n]))
+            header.add_line("##contig=<ID={},length={}, assembly={}>".format(contig, fasta.lengths[n], build))
 
-        header.add_line('##fileDate={}'.format(datetime.now().isoformat()))
-        header.add_line('##sourceVersion={}'.format(sha))
+        # add metadata
+        header.add_line('##filedate={}'.format(datetime.now().isoformat()))
+        header.add_line('##sourceversion={}'.format(sha))
+        if params is not None:
+            for k in params:
+                header.add_line('##{}={}'.format(k, params[k]))
 
         vcf = pysam.VariantFile(path, "w", header=header)
 
@@ -41,7 +41,7 @@ class Vcf:
             record.filter.add(result.vcf_filter)
             record.info['B'] = result.b
             record.info['SE'] = result.se
-            record.info['P'] = result.pval
+            record.info['PVAL'] = result.pval
             record.info['AF'] = result.alt_freq
             record.info['N'] = result.n
             vcf.write(record)
