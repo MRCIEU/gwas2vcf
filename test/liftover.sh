baaa088ee34c23290fc26c36cccb0362ca6f5f6c
Matthew Lyon <ml18692@newblue2.cm.cluster>
2018-12-03 17:57:32 +0000

    tidy lift script

--- test/liftover.sh
+++ test/liftover.sh
@@ -8,9 +8,9 @@ module load apps/bcftools-1.8
 module load apps/gatk-4.0.8.1
 
 # params
-bcf_path="HbA1c_METAL_European.bcf"
-fasta_path="/panfs/panasas01/sscm/ml18692/db/gatk/2.8/b37/human_g1k_v37.fasta"
-chain_path="/panfs/panasas01/sscm/ml18692/db/gatk/chains/b36tob37.chain"
+bcf_path=""
+target_fasta_path=""
+chain_path=""
 
 # convert to vcf
 bcftools view "$bcf_path" > temp.vcf
@@ -18,10 +18,15 @@ bcftools view "$bcf_path" > temp.vcf
 # lift VCF to new build
 gatk LiftoverVcf \
 -I temp.vcf \
--O $(echo "$bcf_path" | sed 's/\.bcf/\.lifted\.bcf/g') \
--R "$fasta_path" \
+-O $(echo "$bcf_path" | sed 's/\.bcf/\.lifted\.vcf/g') \
+-R "$target_fasta_path" \
 --TAGS_TO_DROP B \
 --TAGS_TO_DROP AF \
 -C "$chain_path" \
---REJECT $(echo "$bcf_path" | sed 's/\.vcf/\.rejected\.vcf/g') \
---CREATE_INDEX
+--REJECT $(echo "$bcf_path" | sed 's/\.vcf/\.rejected\.vcf/g')
+
+# convet to bcf
+bcftools view $(echo "$bcf_path" | sed 's/\.bcf/\.lifted\.vcf/g') -Ob -o $(echo "$bcf_path" | sed 's/\.bcf/\.lifted\.bcf/g')
+bcftools index $(echo "$bcf_path" | sed 's/\.bcf/\.lifted\.bcf/g')
+
+rm temp.vcf
