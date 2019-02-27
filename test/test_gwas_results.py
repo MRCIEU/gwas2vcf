47f79554c3d8da44a59919189cc6c34befc10092
Matthew Lyon <ml@eduroam-202-84.nomadic.bris.ac.uk>
2019-02-27 13:57:27 +0000

    updated test

--- test/test_gwas_results.py
+++ test/test_gwas_results.py
@@ -4,9 +4,9 @@ from gwas_results import GwasResult
 
 class TestGwasResults(unittest.TestCase):
     def test_are_alleles_iupac(self):
-        g = GwasResult('1', 100, 'A', 'T', 1, 0.1, 0.1, 100, 0.05, 0, "snp")
+        g = GwasResult('1', 100, 'A', 'T', 1, 0.1, 0.1, 100, 0.05, 0, "snp", None, None)
         self.assertTrue(g.are_alleles_iupac())
-        g = GwasResult('1', 100, 'A', 'wdeT', 1, 0.1, 0.1, 100, 0.05, 0, "snp")
+        g = GwasResult('1', 100, 'A', 'wdeT', 1, 0.1, 0.1, 100, 0.05, 0, "snp", None, None)
         self.assertFalse(g.are_alleles_iupac())
 
 
