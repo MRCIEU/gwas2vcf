8b802f4e46389fb8c8b6e169a44236a166613934
Matthew Lyon <ml@eduroam-202-84.nomadic.bris.ac.uk>
2019-02-27 13:55:40 +0000

    added gz funcs

--- gwas_results.py
+++ gwas_results.py
@@ -1,4 +1,6 @@
 import logging
+import gzip
+import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -73,84 +75,91 @@ class GwasResult:
 
         logging.info("Reading summary stats and mapping to FASTA: {}".format(path))
         total_variants = 0
+        filename, file_extension = os.path.splitext(path)
+
+        if file_extension is '.gz':
+            f = gzip.open(path, 'rt', encoding='utf-8')
+        else:
+            f = open(path, 'r', encoding='utf-8')
 
         results = []
-        with open(path, "r") as f:
-            for n, l in enumerate(f):
+        for n, l in enumerate(f):
 
-                if n < skip_n_rows:
-                    logging.info("Skipping header: {}".format(l.strip()))
-                    continue
+            if n < skip_n_rows:
+                logging.info("Skipping header: {}".format(l.strip()))
+                continue
 
-                total_variants += 1
+            total_variants += 1
 
-                s = l.strip().split("\t")
+            s = l.strip().split("\t")
 
-                chrom = s[chrom_field]
+            chrom = s[chrom_field]
 
-                try:
-                    pos = int(float(s[pos_field]))  # float is for scientific notation
-                except ValueError as e:
-                    logging.warning("Skipping {}: {}".format(s, e))
-                    continue
+            try:
+                pos = int(float(s[pos_field]))  # float is for scientific notation
+            except ValueError as e:
+                logging.warning("Skipping {}: {}".format(s, e))
+                continue
 
-                ref = s[nea_field]
-                alt = s[ea_field]
-                b = float(s[effect_field])
-                se = float(s[se_field])
-                pval = float(s[pval_field])
+            ref = s[nea_field]
+            alt = s[ea_field]
+            b = float(s[effect_field])
+            se = float(s[se_field])
+            pval = float(s[pval_field])
 
-                try:
-                    if ea_af_field is not None:
-                        alt_freq = float(s[ea_af_field])
-                    elif nea_af_field is not None:
-                        alt_freq = 1 - float(s[nea_af_field])
-                    else:
-                        alt_freq = None
-                except (IndexError, TypeError, ValueError):
+            try:
+                if ea_af_field is not None:
+                    alt_freq = float(s[ea_af_field])
+                elif nea_af_field is not None:
+                    alt_freq = 1 - float(s[nea_af_field])
+                else:
                     alt_freq = None
+            except (IndexError, TypeError, ValueError):
+                alt_freq = None
 
-                try:
-                    dbsnpid = s[dbsnp_field]
-                except (IndexError, TypeError, ValueError):
-                    dbsnpid = None
+            try:
+                dbsnpid = s[dbsnp_field]
+            except (IndexError, TypeError, ValueError):
+                dbsnpid = None
 
-                try:
-                    n = float(s[n_field])
-                except (IndexError, TypeError, ValueError):
-                    n = None
+            try:
+                n = float(s[n_field])
+            except (IndexError, TypeError, ValueError):
+                n = None
 
-                try:
-                    prop_cases = float(s[prop_cases_field])
-                except (IndexError, TypeError, ValueError):
-                    prop_cases = None
+            try:
+                prop_cases = float(s[prop_cases_field])
+            except (IndexError, TypeError, ValueError):
+                prop_cases = None
 
-                try:
-                    imp_info = float(s[imp_info_field])
-                except (IndexError, TypeError, ValueError):
-                    imp_info = None
+            try:
+                imp_info = float(s[imp_info_field])
+            except (IndexError, TypeError, ValueError):
+                imp_info = None
 
-                try:
-                    imp_z = float(s[imp_z_field])
-                except (IndexError, TypeError, ValueError):
-                    imp_z = None
+            try:
+                imp_z = float(s[imp_z_field])
+            except (IndexError, TypeError, ValueError):
+                imp_z = None
 
-                result = GwasResult(
-                    chrom,
-                    pos,
-                    ref,
-                    alt,
-                    b,
-                    se,
-                    pval,
-                    n,
-                    alt_freq,
-                    dbsnpid,
-                    prop_cases,
-                    imp_info,
-                    imp_z
-                )
+            result = GwasResult(
+                chrom,
+                pos,
+                ref,
+                alt,
+                b,
+                se,
+                pval,
+                n,
+                alt_freq,
+                dbsnpid,
+                prop_cases,
+                imp_info,
+                imp_z
+            )
 
-                results.append(result)
+            results.append(result)
+
+        f.close()
 
         return results, total_variants
