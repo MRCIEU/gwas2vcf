c01b20136a1abe4267806447ae56744a6adefe5f
Matthew Lyon <matthewlyon@eduroam-191-74.nomadic.bris.ac.uk>
2020-03-11 10:17:37 +0000

    updated test

--- test/test_vcf.py
+++ test/test_vcf.py
@@ -1,5 +1,9 @@
 from vcf import Vcf
 import numpy as np
+import pickle
+from gwas import Gwas
+import tempfile
+from heapq import heappush, heappop
 
 
 def test_convert_pval_to_neg_log10():
@@ -20,3 +24,27 @@ def test_is_valid_float32():
     assert Vcf.is_float32_lossy(-999999999999999999999999999999999999999)
     assert not Vcf.is_float32_lossy(-99999999999999999999999999999999999999)
     assert not Vcf.is_float32_lossy(99999999999999999999999999999999999999)
+
+
+def test_gwas_unpickle():
+    g1 = [Gwas("1", 101, "A", "T", 1, 0, 5e-8, 1000, 0.4, "rs1234", None, None, None),
+          Gwas("1", 105, "A", "T", 1, 0, 5e-8, 1000, 0.4, "rs1234", None, None, None),
+          Gwas("1", 102, "A", "T", 1, 0, 5e-8, 1000, 0.4, "rs1234", None, None, None)]
+    g2 = []
+    idx = []
+    results = tempfile.TemporaryFile()
+    for result in g1:
+        heappush(idx, (result.pos, results.tell()))
+        pickle.dump(result, results)
+
+    while idx:
+        pos = heappop(idx)
+        results.seek(pos[1])
+        result = pickle.load(results)
+        g2.append(result)
+
+    assert g2[0].pos == 101
+    assert g2[1].pos == 102
+    assert g2[2].pos == 105
+
+    results.close()
